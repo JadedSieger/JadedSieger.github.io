b853f665c263ec5a7c0645a2f6c216f8560e0169
Simpcast Productions <66287897+JadedSieger@users.noreply.github.com>
2025-02-21 01:17:13 +0800

    sendCommand

--- ControlPanel/control-panel.js
+++ ControlPanel/control-panel.js
@@ -1,6 +1,6 @@
 const API_URL = "https://lieserl-einstein-js-production.up.railway.app"; // Ensure HTTPS
 
-function sendCommand(command) {
+function SendCommand(command) {
     fetch(`${API_URL}/bot/${command}`, { method: "POST" })
         .then(response => response.json())
         .then(data => alert(data.message))    
