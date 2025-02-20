3f902cbfaab2c419b3907c48a6eb8e83a9de24c4
Simpcast Productions <66287897+JadedSieger@users.noreply.github.com>
2025-02-20 23:40:26 +0800

    Update control-panel.js

--- ControlPanel/control-panel.js
+++ ControlPanel/control-panel.js
@@ -3,6 +3,13 @@ const API_URL = "";
 function SendCommand(command) {
     fetch(`${API_URL}/bot/${command}`, {method: "POST"})
     .then(response => response.json())
+    .then(data => alert(data.message))    
+    .catch(error => console.error("Error: ", error));
+}
+
+function getStatus(command) {
+    fetch(`${API_URL}/bot/${command}`, {method: "POST"})
+    .then(response => response.json())
     .then(data => {
         document.getElementById("status").textContent = "Status: " + data.status;
     })
